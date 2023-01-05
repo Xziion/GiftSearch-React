6161b7c318ced3c4ebf0292aaef5c08c25f14565
Xziion <rivera.ascencio@hotmail.com>
2023-01-05 12:43:54 -0600

    trying deploy on netlify

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,8 +1,9 @@
 export const getGifs = async (category) => {
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=STAtC8OMo5y5ijIpx73UG7PpMCBkqIkn&q=${category}&limit=14`;
-    const resp = await fetch(url);
+
+    //const url = `https://api.giphy.com/v1/gifs/search?api_key=STAtC8OMo5y5ijIpx73UG7PpMCBkqIkn&q=${category}&limit=14`;
+    const resp = await fetch(`https://api.giphy.com/v1/gifs/search?q=${category}&api_key=STAtC8OMo5y5ijIpx73UG7PpMCBkqIkn&limit=10`);
     const { data } = await resp.json();
-    
+
     const gifs = data.map((img) => ({ 
     id: img.id,
     title: img.title,
